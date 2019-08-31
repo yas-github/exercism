e9505436dce65f1f3c64ba490877165fbba59f88
yas <the-yas.hoochie_koo@hotmail.co.jp>
2019-08-31 12:57:17 +0000

    side.11 series -v2

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -1,13 +1,13 @@
 class Series
-  attr_reader :str
-
   def initialize(str)
     @str = str
   end
 
   def slices(digits)
     raise ArgumentError if str.length < digits
-
     str.chars.each_cons(digits).map(&:join)
   end
+
+  private
+  attr_reader :str
 end
