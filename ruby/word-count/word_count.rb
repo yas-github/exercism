7389f98a5f488813e0e51b4126b5ab63d7ef01e1
yas <the-yas.hoochie_koo@hotmail.co.jp>
2019-09-24 22:08:52 +0000

    core.9 word_count v1

---
+++ ruby/word-count/word_count.rb
@@ -0,0 +1,15 @@
+class Phrase
+  attr_reader :words
+
+  def initialize(phrase)
+    phrase = phrase.gsub(/[\n\.\$\^:,!&@%(?<= )''(?= )]/, " ")
+    @words = phrase.split(" ").map(&:downcase)
+  end
+
+  def word_count
+    counts = {}
+    words.each { |word| counts[word] = words.count(word) }
+
+    counts
+  end
+end
