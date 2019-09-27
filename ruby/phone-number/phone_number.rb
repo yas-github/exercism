0d9bb63042ff10f6bace0f9f2c6b5d0f8b77c887
yas <the-yas.hoochie_koo@hotmail.co.jp>
2019-09-27 06:21:45 +0000

    side.13 phone-number v2

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -2,10 +2,12 @@ class PhoneNumber
   def self.clean(num)
     code = num.gsub(/^1|^\+1|[^\d]/, " ").split
     number = code.join
+    area_code = code[0].chars[0]
+    exchange_code = code[1].chars[0] unless code[1].nil?
 
     return nil if number.length != 10
-    return nil if %w(0 1).include?(code[0].chars[0])
-    return nil if %w(0 1).include?(code[1].chars[0])
+    return nil if %w(0 1).include?(area_code)
+    return nil if %w(0 1).include?(exchange_code)
     number
   rescue
     number
