61e7efd068717ac5a8a9bb2d33835ed1966966c7
yas <the-yas.hoochie_koo@hotmail.co.jp>
2019-11-07 23:50:03 +0000

    [word-count] 1st refactoring

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,15 +1,17 @@
 class Phrase
   attr_reader :words
+  # REGEX = /[^\b[\w']+\b]/
+  REGEX = /[\n\.\$\^:,!&@%]|(?<= )'|'(?= )/
 
   def initialize(phrase)
-    phrase = phrase.gsub(/[\n\.\$\^:,!&@%]|(?<= )'|'(?= )/, " ")
+    phrase = phrase.gsub(REGEX, " ")
     @words = phrase.split(" ").map(&:downcase)
   end
 
   def word_count
-    counts = {}
-    words.each { |word| counts[word] = words.count(word) }
-
+    words.each_with_object(counts = {}) do |word|
+      counts[word] = words.count(word)
+    end
     counts
   end
 end
